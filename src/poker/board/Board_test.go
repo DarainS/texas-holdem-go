43a78d791ea6182723f9ac33b5bda3eb1fbcc016
Darain <d29702@163.com>
2017-11-17 16:06:17 +0800

    add Board_test.go

--- src/poker/board/Board_test.go
+++ src/poker/board/Board_test.go
@@ -11,17 +11,17 @@ func TestBoard_FromString(t *testing.T) {
 		s string
 	}
 	tests := []struct {
-		name string
-		deck Board
-		args args
-		want Board
+		name  string
+		board Board
+		args  args
+		want  Board
 	}{
 	// TODO: Add test cases.
 
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := tt.deck.FromString(tt.args.h, tt.args.s); !reflect.DeepEqual(got, tt.want) {
+			if got := tt.board.FromString(tt.args.h, tt.args.s); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("Board.FromString() = %v, want %v", got, tt.want)
 			}
 		})
